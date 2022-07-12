bab2117752be6a7dc4bde795b5bb0e1ac25eaf60
Nhi Nguyen <nhi_nguyen1610@hotmail.com>
2022-07-12 13:56:20 +1000

    Fix bug related to getNewDeck

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const cardValue = [
   "ACE",
 ];
 
-async function getNewDeck() {
+function getNewDeck() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
